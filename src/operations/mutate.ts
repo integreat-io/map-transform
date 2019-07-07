be156496ddb5a68870b7d778632e595945d749d9
Kjell-Morten <post@kjellmorten.no>
2019-07-07 23:29:53 +0200

    Update map-any

--- src/operations/mutate.ts
+++ src/operations/mutate.ts
@@ -1,20 +1,21 @@
-import * as mapAny from 'map-any'
+import mapAny = require('map-any')
 import { Operation, State, MapObject, Options } from '../types'
 import { getStateValue, setStateValue } from '../utils/stateHelpers'
 import objectToMapFunction from '../utils/objectToMapFunction'
 
-export default function mutate (def: MapObject): Operation {
+export default function mutate(def: MapObject): Operation {
   return (options: Options) => {
     const runMutation = objectToMapFunction(def, options)
 
-    return (state: State): State => (typeof state.value === 'undefined')
-    ? state
-    : setStateValue(
-      state,
-      mapAny(
-        (value) => getStateValue(runMutation(setStateValue(state, value))),
-        state.value
-      )
-    )
+    return (state: State): State =>
+      typeof state.value === 'undefined'
+        ? state
+        : setStateValue(
+            state,
+            mapAny(
+              value => getStateValue(runMutation(setStateValue(state, value))),
+              state.value
+            )
+          )
   }
 }

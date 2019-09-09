686671dce3c1c872ef76bf9fb22711c738b21a26
Kjell-Morten <post@kjellmorten.no>
2019-09-09 11:10:07 +0200

    Workaround bug with tsconfig root

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:promise/recommended',
+    'plugin:security/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'promise', 'security', 'node'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', ignoreRestSiblings: true }
+    ],
+    'node/no-extraneous-import': [
+      'error',
+      {
+        tryExtensions: ['.js', '.json', '.ts']
+      }
+    ]
+  }
+}

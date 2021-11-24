f90025714700017c0fea04eea64882f5e165ec95
Kjell-Morten <post@kjellmorten.no>
2021-11-24 08:03:49 +0100

    Use Mustache instead of Handlebars

--- src/functions/template.ts
+++ src/functions/template.ts
@@ -1,4 +1,4 @@
-import Handlebars = require('handlebars')
+import Mustache = require('mustache')
 import mapAny = require('map-any')
 import getter from '../utils/pathGetter'
 import { Path, DataMapper } from '../types'
@@ -11,13 +11,18 @@ interface Operands {
 const extractOperands = (operands: Operands | string) =>
   typeof operands === 'string' ? { template: operands } : operands
 
+function parseAndCreateGenerator(templateStr: string) {
+  Mustache.parse(templateStr) // Mustache will keep the parsed template in a cache
+  return (data: unknown) =>
+    mapAny((data: unknown) => Mustache.render(templateStr, data), data)
+}
+
 export default function template(operands: Operands | string): DataMapper {
   const { template: templateStr, templatePath } = extractOperands(operands)
 
   if (typeof templateStr === 'string') {
-    // We already got a template -- return a generator
-    const generate = Handlebars.compile(templateStr)
-    return (data) => mapAny((data: unknown) => generate(data), data)
+    // We already got a template -- preparse it and return a generator
+    return parseAndCreateGenerator(templateStr)
   } else if (typeof templatePath === 'string') {
     // The template will be provided in the data -- return a function that will
     // both create the generator and run it
@@ -25,8 +30,7 @@ export default function template(operands: Operands | string): DataMapper {
     return (data) => {
       const templateStr = getFn(data)
       if (typeof templateStr === 'string') {
-        const generate = Handlebars.compile(templateStr)
-        return mapAny((data: unknown) => generate(data), data)
+        return parseAndCreateGenerator(templateStr)(data)
       }
       return undefined
     }

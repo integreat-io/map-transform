6f1be7faadde54403661b5d1ed823a1532c1039e
Kjell-Morten <post@kjellmorten.no>
2022-08-25 16:08:05 +0200

    Remove flip only for applied pipeline - and not the next call

--- src/operations/apply.ts
+++ src/operations/apply.ts
@@ -1,5 +1,6 @@
 import { Options, Operation, State } from '../types'
 import { operationFromDef } from '../utils/definitionHelpers'
+import { identity } from '../utils/functional'
 
 const extractPipeline = (pipelineId: string, { pipelines }: Options) =>
   pipelineId && pipelines ? pipelines[pipelineId] : undefined // eslint-disable-line security/detect-object-injection
@@ -9,7 +10,12 @@ const removeFlip = ({ flip, ...state }: State) => state
 export default function apply(pipelineId: string): Operation {
   return (options) => (next) => {
     const pipeline = extractPipeline(pipelineId, options)
-    const fn = pipeline ? operationFromDef(pipeline)(options)(next) : next
-    return (state) => fn(removeFlip(state))
+    const fn = pipeline
+      ? operationFromDef(pipeline)(options)(identity)
+      : undefined
+    return (state) => {
+      const nextState = next(state)
+      return fn ? fn(removeFlip(nextState)) : nextState
+    }
   }
 }

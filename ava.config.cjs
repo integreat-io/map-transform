24d256b3439ae224dd2de6e180527555f7383d98
Kjell-Morten <post@kjellmorten.no>
2023-01-15 21:42:59 +0100

    Only transpile typescript in tests

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extensions: { ts: 'module' },
+  extensions: { js: true, ts: 'module' },
   nodeArguments: ['--loader=ts-node/esm', '--no-warnings'],
   ignoredByWatcher: ['{.nyc_output,dist,media}/**'],
   files: ['src/**/*.test.ts'],

fcfd9025a745e95994d9d26a42c5ade777308ebf
Kjell-Morten <post@kjellmorten.no>
2024-03-14 14:29:52 +0100

    Update dev env

---
+++ ava.config.js
@@ -0,0 +1,11 @@
+export default {
+  environmentVariables: {
+    TSIMP_DIAG: 'ignore',
+  },
+  extensions: { js: true, ts: 'module' },
+  nodeArguments: ['--import=tsimp'],
+  watchMode: {
+    ignoreChanges: ['{.nyc_output,dist,media,.tsimp}/**', '*.md'],
+  },
+  files: ['src/**/*.test.ts'],
+}

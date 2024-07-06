20b93a13cad3a1bff5618436230f1c846781e7ac
Kjell-Morten <post@kjellmorten.no>
2024-07-06 13:03:05 +0200

    Simplify alt operation

--- src/operations/alt.ts
+++ src/operations/alt.ts
@@ -28,12 +28,6 @@ const pipeIfArray = (def: NextStateMapper | NextStateMapper[]) =>
 // the state untouched when we move in the wrong direction. We don't want the
 // untouched pipeline here, as a skipped pipeline should be treated as if it
 // returned a nonvalue.
-//
-// Note that we're just checking that the values are the exact same here, which
-// may not be work in all cases. E.g. the pipeline `'.'` would be considered
-// untouched, even though it is not wrapped in a directional, but again, what
-// would be the use of having a pipeline that just returns the state untouched
-// in an alt operation?
 async function runAltPipeline(pipeline: StateMapper, state: State) {
   const afterState = await pipeline(markAsUntouched(state))
   return isUntouched(afterState)
@@ -41,14 +35,60 @@ async function runAltPipeline(pipeline: StateMapper, state: State) {
     : afterState
 }
 
+// Run the first alt pipeline. If it returns a nonvalue, we push the context to
+// make the original value available to the following pipelines. Pushing context
+// also makes the parent path work as expected after the alt if all alt
+// pipelines return nonvalues.
+async function runAltFirst(
+  state: State,
+  pipeline: StateMapper,
+  nonvalues?: unknown[],
+) {
+  // Run operation and set value if it is not a nonvalue
+  const afterState = await runAltPipeline(pipeline, state)
+  return isNonvalueState(afterState, nonvalues)
+    ? setValueFromState(state, afterState, true) // We got a non-value, so set it on the original state
+    : afterState // We got a value, so return it
+}
+
+// Run a pipeline that is not the first. We pop the context pushed by the first
+// pipeline to get the original value if the first alt pipeline returned a
+// nonvalue.
+async function runAltRest(
+  state: State,
+  pipeline: StateMapper,
+  nonvalues?: unknown[],
+) {
+  if (!isNonvalueState(state, nonvalues)) {
+    // We already have a value, so we don't need to run the alt operation
+    return state
+  }
+
+  // For all alts after the first, we remove the context pushed from the
+  // first and provides it as the value.
+  const beforeState = popContext(state)
+
+  // Run operation and set value if it is not a nonvalue
+  const afterState = await runAltPipeline(pipeline, beforeState)
+  return isNonvalueState(afterState, nonvalues)
+    ? setValueFromState(state, afterState) // We got a non-value, so set it on the original state
+    : afterState // We got a value, so return it
+}
+
+const createAltFn = (
+  next: StateMapper,
+  pipeline: StateMapper,
+  isFirst: boolean,
+  nonvalues?: unknown[],
+) =>
+  async function runAlt(state: State) {
+    const nextState = await next(state)
+    const fn = isFirst ? runAltFirst : runAltRest
+    return await fn(nextState, pipeline, nonvalues)
+  }
+
 // Prepare the pipeline and return an operation that will run it if the state
 // value is a nonvalue or if this is the first of several alt pipelines.
-//
-// The way we push and pop context might seem a bit strange, but the logic is
-// that if the first alt pipeline returns a nonvalue, the pushed context makes
-// the original value available to the following pipelines. Also, if all return
-// nonvalues, it is correct that we have moved one level down and need to go up
-// again to get the value from before the alt operation.
 function createOneAltPipeline(
   def: TransformDefinition,
   index: number,
@@ -57,25 +97,8 @@ function createOneAltPipeline(
   return (options) => {
     const pipeline = pipeIfArray(defToNextStateMappers(def, options))(noopNext)
     const isFirst = !hasOnlyOneAlt && index === 0
-    const { nonvalues } = options
-
-    return (next: StateMapper) => async (state: State) => {
-      const nextState = await next(state)
-      if (!isFirst && !isNonvalueState(nextState, nonvalues)) {
-        // We already have a value, so we don't need to run the alt operation
-        return nextState
-      }
-
-      // For all alts after the first, we remove the context pushed from the
-      // first and provides it as the value.
-      const beforeState = isFirst ? nextState : popContext(nextState)
-
-      // Run operation and set value if it is not a nonvalue
-      const afterState = await runAltPipeline(pipeline, beforeState)
-      return isNonvalueState(afterState, nonvalues)
-        ? setValueFromState(nextState, afterState, isFirst) // We got a non-value, so set it on the original state
-        : afterState // We got a value, so return it
-    }
+    return (next: StateMapper) =>
+      createAltFn(next, pipeline, isFirst, options.nonvalues)
   }
 }
 

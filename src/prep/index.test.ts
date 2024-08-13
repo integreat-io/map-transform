869b962b9dc3d2881ab835858353a0530249eeaf
Kjell-Morten <post@kjellmorten.no>
2024-08-13 16:43:17 +0200

    Flatten out sub-pipelines

---
+++ src/prep/index.test.ts
@@ -0,0 +1,113 @@
+import test from 'ava'
+import type { State } from '../types.js'
+
+import prep from './index.js'
+
+// Setup
+
+const uppercaseFn = (val: unknown, _state: State) =>
+  typeof val === 'string' ? val.toUpperCase() : val
+const uppercase = () => () => uppercaseFn
+
+const options = {
+  transformers: { uppercase },
+  fwdAlias: 'from',
+  revAlias: 'to',
+}
+
+// Tests
+
+test('should prepare pipeline', (t) => {
+  const def = ['data.name', { $transform: 'uppercase' }, '>items[].title']
+  const expected = [
+    'data',
+    'name',
+    { type: 'transform', fn: uppercaseFn },
+    '>title',
+    '>[]',
+    '>items',
+  ]
+
+  const ret = prep(def, options)
+
+  t.deepEqual(ret, expected)
+})
+
+test('should prepare pipeline with sub-pipeline', (t) => {
+  const def = ['data', ['name', { $transform: 'uppercase' }], '>items[].title']
+  const expected = [
+    'data',
+    'name',
+    { type: 'transform', fn: uppercaseFn },
+    '>title',
+    '>[]',
+    '>items',
+  ]
+
+  const ret = prep(def, options)
+
+  t.deepEqual(ret, expected)
+})
+
+test('should prepare transform operation with iteration', (t) => {
+  const def = [
+    {
+      $transform: 'uppercase',
+      $iterate: true,
+    },
+  ]
+  const expected = [{ type: 'transform', fn: uppercaseFn, it: true }]
+
+  const ret = prep(def, options)
+
+  t.deepEqual(ret, expected)
+})
+
+test('should prepare transform operation with direction fwd', (t) => {
+  const def = {
+    $transform: 'uppercase',
+    $direction: 'fwd',
+    $iterate: true,
+  }
+  const expected = [{ type: 'transform', fn: uppercaseFn, dir: 1, it: true }]
+
+  const ret = prep(def, options)
+
+  t.deepEqual(ret, expected)
+})
+
+test('should prepare transform operation with direction rev', (t) => {
+  const def = {
+    $transform: 'uppercase',
+    $direction: 'rev',
+  }
+  const expected = [{ type: 'transform', fn: uppercaseFn, dir: -1 }]
+
+  const ret = prep(def, options)
+
+  t.deepEqual(ret, expected)
+})
+
+test('should prepare transform operation with direction set to fwdAlias', (t) => {
+  const def = {
+    $transform: 'uppercase',
+    $direction: 'from',
+  }
+  const expected = [{ type: 'transform', fn: uppercaseFn, dir: 1 }]
+
+  const ret = prep(def, options)
+
+  t.deepEqual(ret, expected)
+})
+
+test('should prepare transform operation with direction revAlias', (t) => {
+  const def = {
+    $transform: 'uppercase',
+    $direction: 'to',
+  }
+  const expected = [{ type: 'transform', fn: uppercaseFn, dir: -1 }]
+
+  const ret = prep(def, options)
+
+  t.deepEqual(ret, expected)
+})

c938ede05611fa92ddda3a5978162674ba84511f
Kjell-Morten <post@kjellmorten.no>
2024-08-14 12:45:50 +0200

    Implement mutation object

---
+++ src/prep/mutation.ts
@@ -0,0 +1,53 @@
+import prepPipeline, { Def } from './index.js'
+import preparePathStep from './path.js'
+import { isNotNullOrUndefined, isObject } from '../utils/is.js'
+import type { MutationStep } from '../run/mutation.js'
+import type { Options, MutationObject } from '../types.js'
+
+// Prepare one property by setting the key as the set path at the end of the
+// pipeline. Properties starting with `'$'` or properties withtout pipelines
+// are not included. Properties ending in `'[]'` with another mutation object
+// as pipeline, is iterated.
+function prepProp(setPath: string, pipeline: Def, options: Options) {
+  if (setPath.startsWith('\\$')) {
+    setPath = setPath.slice(1)
+  } else if (setPath[0] === '$' || !pipeline) {
+    return undefined
+  } else if (setPath.endsWith('[]') && isObject(pipeline)) {
+    pipeline = { ...pipeline, $iterate: true }
+  }
+  return [...prepPipeline(pipeline, options), ...preparePathStep(`>${setPath}`)]
+}
+
+/**
+ * Prepare a mutation step and return the internal step format. Each property
+ * on the mutation object is made into a pipeline, with the prop as set path at
+ * the end. `$modify` and `$flip` props are set on the step object as `mod` and
+ * `flip`. `$modify` is prepared as a pipeline of only path segments, with
+ * `true` becomming an empty pipeline.
+ */
+export default function prepareMutationStep(
+  { $modify, $flip: flip = false, ...props }: MutationObject,
+  options: Options,
+): MutationStep | undefined {
+  const pipelines = Object.entries(props)
+    .map(([setPath, pipeline]) => prepProp(setPath, pipeline as Def, options))
+    .filter(isNotNullOrUndefined)
+  if (pipelines.length === 0 && $modify === true) {
+    return undefined
+  }
+
+  const mod =
+    $modify === true
+      ? []
+      : typeof $modify === 'string'
+        ? preparePathStep($modify)
+        : undefined
+
+  return {
+    type: 'mutation',
+    ...(flip && { flip }),
+    ...(mod && { mod }),
+    pipelines,
+  }
+}
--- src/run/mutation.ts
+++ src/run/mutation.ts
@@ -1,22 +1,48 @@
 import runPipeline, { PreppedPipeline } from './index.js'
-import type { State } from '../types.js'
+import { isObject } from '../utils/is.js'
+import type { Path, State } from '../types.js'
 
 export interface MutationStep {
   type: 'mutation'
+  flip?: boolean
+  mod?: Path[]
   pipelines: PreppedPipeline[]
 }
 
+/**
+ * Run a mutation step, by running each pipeline and combining the results
+ * into one object. This is done by giving the result of a pipeline as the
+ * target for the next. If a `mod` pipeline is given, we'll shallow merge the
+ * value it's pointing to with the result from the pipelines, as long as both
+ * are objects.
+ */
 export default function runMutationStep(
   value: unknown,
-  { pipelines }: MutationStep,
+  { pipelines, mod: modPipeline, flip: ourFlip }: MutationStep,
   state: State,
 ) {
-  return pipelines.reduce(
+  const flip = typeof ourFlip === 'boolean' ? ourFlip : state.flip
+  // Run every pipeline in turn, with the result of the previous as the target
+  // of the next. The first one is given an empty object as target
+  const next = pipelines.reduce(
     (target, pipeline) =>
-      runPipeline(value, pipeline, { ...state, target }) as Record<
+      runPipeline(value, pipeline, { ...state, target, flip }) as Record<
         string,
         unknown
       >,
     {},
   )
+
+  // If we have a mod pipeline and next is an object, we get the value at the
+  // mod pipeline and shallow merge with it, if it's an object too
+  if (modPipeline && isObject(next)) {
+    // TODO: Override direction to fwd, here?
+    const modValue = runPipeline(value, modPipeline, state) // TODO: Use a get path only runner instead of running a full pipeline
+    if (isObject(modValue)) {
+      return { ...modValue, ...next }
+    }
+  }
+
+  // We're not merging with a mod object -- just return
+  return next
 }

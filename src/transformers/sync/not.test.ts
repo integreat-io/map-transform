b8609b8e0130e6ee11efbaf56886288e6fbc57ef
Kjell-Morten <post@kjellmorten.no>
2024-08-16 21:56:54 +0200

    Make available synchronous transformers

---
+++ src/transformers/sync/not.test.ts
@@ -0,0 +1,47 @@
+import test from 'ava'
+import State from '../../state.js'
+
+import not from './not.js'
+
+// Setup
+
+const options = {}
+const state = new State()
+
+// Tests
+
+test('should return true for false', (t) => {
+  const value = false
+  const expected = true
+
+  const ret = not({})(options)(value, state)
+
+  t.is(ret, expected)
+})
+
+test('should return false for true', (t) => {
+  const value = true
+  const expected = false
+
+  const ret = not({})(options)(value, state)
+
+  t.is(ret, expected)
+})
+
+test('should return true for falsy', (t) => {
+  const value = null
+  const expected = true
+
+  const ret = not({})(options)(value, state)
+
+  t.is(ret, expected)
+})
+
+test('should return false for truthy', (t) => {
+  const value = 'true'
+  const expected = false
+
+  const ret = not({})(options)(value, state)
+
+  t.is(ret, expected)
+})
---
+++ src/transformers/sync/not.ts
@@ -0,0 +1,8 @@
+import type { Transformer } from '../../types.js'
+
+const transformer: Transformer = () => () =>
+  function not(value, _state) {
+    return !value
+  }
+
+export default transformer

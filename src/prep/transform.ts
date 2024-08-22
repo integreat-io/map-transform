7c5201f10d996df00db7b103ebe31651f7e0b860
Kjell-Morten <post@kjellmorten.no>
2024-08-22 12:14:13 +0200

    Improve error messages on unknown transformer

--- src/prep/transform.ts
+++ src/prep/transform.ts
@@ -2,19 +2,23 @@ import type { TransformStep } from '../run/transform.js'
 import type { TransformOperation, Options } from '../types.js'
 import type { Options as OptionsNext } from './index.js'
 
-const getTransformerFn = (fnId: string | symbol, options: OptionsNext) =>
-  options.transformers && options.transformers[fnId] // eslint-disable-line security/detect-object-injection
-
 export default function prepareTransformStep(
-  { $transform, ...props }: TransformOperation,
+  { $transform: id, ...props }: TransformOperation,
   options: OptionsNext,
 ): TransformStep {
-  const fn = getTransformerFn($transform, options)
+  if (!id) {
+    throw new Error('Transformer operation is missing transformer id')
+  }
+  if (!options.transformers) {
+    throw new Error(
+      `Transformer '${String(id)}' was not found. No transformers`,
+    )
+  }
+
+  const fn = options.transformers[id] // eslint-disable-line security/detect-object-injection
   if (typeof fn === 'function') {
     return { type: 'transform', fn: fn(props)(options as Options) }
   } else {
-    throw new Error(
-      'Transform operation was called without a valid transformer function',
-    )
+    throw new Error(`Transformer '${String(id)}' was not found`)
   }
 }
